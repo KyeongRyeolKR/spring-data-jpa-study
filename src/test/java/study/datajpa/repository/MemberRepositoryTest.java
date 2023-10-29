3e1711c33a8585b280648bd2b3cdafbd28c4a6fd
김경렬 <rlarudfuf802@naver.com>
2023-10-29 15:18:23 +0900

    반환 타입

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Team;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -150,5 +151,27 @@ class MemberRepositoryTest {
         for (Member member : result) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> list = memberRepository.findListByUsername("AAA");
+        System.out.println("========= list =========");
+        for (Member member : list) {
+            System.out.println("member = " + member);
+        }
+
+        Member one = memberRepository.findMemberByUsername("AAA");
+        System.out.println("========= one =========");
+        System.out.println("member = " + one);
+
+        Optional<Member> optional = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("========= optional =========");
+        System.out.println("member = " + optional);
     }
 }
